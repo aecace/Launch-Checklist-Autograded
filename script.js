9a70b61ce988ce03b48ef595cc76df88585c1143
Amanda Crawford <crawforda0502@gmail.com>
2024-03-19 02:56:39 -0500

    FINALLY unbroke Task 2, redo of task 3 to go

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
 // Write your JavaScript code here!
 
+const { formSubmission } = require("./scriptHelper");
+
+//Add an event handler for the window load event
 window.addEventListener("load", function() {
 
+    // TASK 2
+    //Within the window’s load handler, add an event handler for the form submit event
+    let form = document.querySelector("form");
+    form.addEventListener("submit", function(event) {
+        //prevent the form submission from happening until all inputs have valid values
+        event.preventDefault();
+
+        //Retrieve input values that need to be validated from the DOM.
+        let pilot = document.querySelector("input[name=pilotName]").value;
+        let copilot = document.querySelector("input[name=copilotName]").value;
+        let fuelLevel = document.querySelector("input[name=fuelLevel]").value;
+        let cargoMass = document.querySelector("input[name=cargoMass]").value;
+        let list = document.getElementById("faultyItems");
+
+        //Make sure to call your formSubmission() function at the appropriate time in your script.js file!
+        formSubmission(document, list, pilot, copilot, fuelLevel, cargoMass);
+    })
+
+    // TASK 5
     let listedPlanets;
     // Set listedPlanetsResponse equal to the value returned by calling myFetch()
     let listedPlanetsResponse;
--- scriptHelper.js
+++ scriptHelper.js
@@ -23,16 +23,22 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
     // takes in a string as a parameter
     // ex: isNaN(fuelLevel);
     // return "Empty", "Not a Number", or "Is a Number" as appropriate
+    
+    // if (testInput === "" || isNaN(testInput) || !isNaN(testInput)) {
+    //     alert("All fields are required!");
+    //     event.preventDefault();
+    // }
 
-    // Alert: all fields are required.
-    // User entered valid info for each of the fields (easily converted to correct data type).
-        // pilot and co-pilot names should be strings
-        // fuel level and cargo mass should be numbers
-
-
+    if (testInput === "") {
+        return "Empty";
+    } else if (isNaN(testInput)) {
+        return "Not a Number";
+    } else if (!isNaN(testInput)) {
+        return "Is a Number";
+    }
  }
  
- function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
+ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoMass) {
     // TASK 2, majority of code
     // function to call after form submission
 
@@ -40,7 +46,55 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
     // if statements
     // use validateInput() to complete
     // ex: validateInput(pilot) returns that pilot is not an empty string;
+
+    // Alert: all fields are required.
+    // User entered valid info for each of the fields (easily converted to correct data type).
+        // pilot and co-pilot names should be strings
+        // fuel level and cargo mass should be numbers
+    if (validateInput(pilot) === "Empty" && validateInput(copilot) === "Empty" && validateInput(fuelLevel) === "Empty" && validateInput(cargoMass) === "Empty") {
+        alert("All fields are required!");
+    } else if (validateInput(pilot) !== "Not a Number" && validateInput(copilot) !== "Not a Number" && validateInput(fuelLevel) !== "Is a Number" && validateInput(cargoMass) !== "Is a Number") {
+        alert("Enter valid info for each of the fields!");
+    }
+
+
     // change the color and text of items, change visibility style of items from html, etc.
+    // declare list items
+    let listPilotStatus = document.getElementById("pilotStatus");
+    let listCopilotStatus = document.getElementById("copilotStatus");
+    let listFuelStatus = document.getElementById("fuelStatus");
+    let listCargoStatus = document.getElementById("cargoStatus");
+    let listLaunchStatus = document.getElementById("launchStatus");
+    let readyToLaunch = true;
+    //update the li element pilotStatus to include the pilot’s name
+    listPilotStatus.innerHTML = `Pilot ${pilot} is ready for launch`;
+    //update the li element copilotStatus to include the co-pilot’s name
+    listCopilotStatus.innerHTML = `Co-pilot ${copilot} is ready for launch`;
+    //fuel level<10,000 --> change faultyItems to visible + update fuel status to "there is not enough fuel for the journey"
+    if (Number(fuelLevel) < 10000) {
+        list.style.visibility = "visible";
+        listFuelStatus.innerHTML = `Fuel level too low for launch`;
+        listLaunchStatus.innerHTML = `Shuttle Not Ready for Launch`;
+        listLaunchStatus.style.color = "red";
+        readyToLaunch = false;
+    } else {
+        listFuelStatus.innerHTML = `Fuel level high enough for launch`;
+    }
+    //cargo mass>10,000 --> change faultyItems to visible + update cargo status to "there is too much mass for the shuttle to take off"
+    if (Number(cargoMass) > 10000) {
+        list.style.visibility = "visible";
+        listCargoStatus.innerHTML = `Cargo mass too heavy for launch`;
+        listLaunchStatus.innerHTML = `Shuttle Not Ready for Launch`;
+        listLaunchStatus.style.color = "red";
+        readyToLaunch = false;
+    } else {
+        listCargoStatus.innerHTML = `Cargo mass low enough for launch`;
+    }
+    //If the shuttle is ready to launch, change the text of launchStatus to green and display “Shuttle is ready for launch”.
+    if (readyToLaunch === true) {
+        listLaunchStatus.innerHTML = `Shuttle is Ready for Launch`;
+        listLaunchStatus.style.color = `green`;
+    }
  }
  
  // TASK 3
